cf2ba09e1eb1f20abf9e1886178a7f0be7db38d3
chowkingman <>
2022-06-20 12:19:10 -0500

    updates2

--- server.test.js
+++ server.test.js
@@ -11,3 +11,55 @@ describe("Test the root path", () => {
       });
   });
 });
+
+describe("Test GET /qa/questions", () => {
+  test("It should response the GET method", () => {
+    return request(app)
+      .get("/qa/questions")
+      .query({
+        page: 1,
+        count: 10,
+        product_id: 10010
+      })
+      .expect(200);
+  });
+});
+
+describe("Test GET /qa/questions/:question_id/answers", () => {
+  test("It should response the GET method", () => {
+    return request(app)
+      .get("/qa/questions/:question_id/answers")
+      .query({
+        question_id: 10010
+      })
+      .expect(200);
+  });
+});
+
+describe("Test POST /qa/questions", () => {
+  test("It should response the POST method", () => {
+    return request(app)
+      .post("/qa/questions")
+      .query({
+        product_id: 10010,
+        body: "hi",
+        name: 'piggy',
+        email: "google.com"
+      })
+      .expect(201);
+  });
+});
+
+describe("Test POST /qa/questions/:question_id/answers", () => {
+  test("It should response the POST method", () => {
+    return request(app)
+      .post("/qa/questions/:question_id/answers")
+      .query({
+        question_id: 10010,
+        body: "hi",
+        name: 'piggy',
+        email: "google.com"
+      })
+      .expect(201);
+  });
+});
