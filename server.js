04ab9f9269c341ed536c5e99a8e606cd2edb0fd9
chowkingman <>
2022-06-24 10:34:19 -0500

    updates for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const app = require("./app");
-const port = 3020;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`QnA Server listening on port ${port}!`);
